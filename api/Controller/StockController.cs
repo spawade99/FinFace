0f38896e3a7f443f51a9710466cef89c5ae56614
Shubham Pawade <Shubham.Pawade@bentley.com>
2025-05-12 12:45:33 +0530

    Added  stock Update and Delete endpoints

--- api/Controller/StockController.cs
+++ api/Controller/StockController.cs
@@ -46,5 +46,44 @@ namespace api.Controller
             return CreatedAtAction(nameof(GetStock), new { id = stock.Id }, stock.ToStockDto());
         }
 
+        [HttpPut]
+        [Route("UpdateStock/{id}")]
+        public async Task<IActionResult> UpdateStock([FromRoute] int id, [FromBody] CreateStockDto stockDto)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            stock.Symbol = stockDto.Symbol;
+            stock.CompanyName = stockDto.CompanyName;
+            stock.Industry = stockDto.Industry;
+            stock.LastDiv = stockDto.LastDiv;
+            stock.MarketCap = stockDto.MarketCap;
+            stock.Purchase = stockDto.Purchase;
+
+            // _context.Update(stock);
+            await _context.SaveChangesAsync();
+
+            return Ok(stock.ToStockDto());
+
+        }
+
+        [HttpDelete]
+        [Route("DeleteStock/{id}")]
+        public async Task<ActionResult> DeleteStock([FromRoute] int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
