e0dfa3791b63e3b357b4b73d09234d7fee8765e1
Shubham Pawade <Shubham.Pawade@bentley.com>
2025-05-12 08:50:24 +0530

    Created webapi and added models and copied existing react app

---
+++ api/Models/Comment.cs
@@ -0,0 +1,16 @@
+namespace api.Models
+{
+    public class Comment
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int? StockId { get; set; }
+
+        //Navigation property
+        public Stock? Stock { get; set; }
+    }
+}
